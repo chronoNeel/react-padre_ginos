bcb7304d304b025482bf7a7f44ec7ea68f57a7f9
Niloy Ro <niloy.roy@therapservices.net>
2025-11-06 16:04:35 +0600

    prettier and gitignore

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
 const Pizza = (props) => {
-  return React.createElement("div", {}, [
+  return React.createElement('div', {}, [
     React.createElement(
-      "div",
-      { style: { display: "flex", gap: "100px", alignItems: "center" } },
+      'div',
+      {
+        style: {
+          display: 'flex',
+          gap: '100px',
+          alignItems: 'center',
+        },
+      },
+
       [
-        React.createElement("p", {}, "Pizza:"),
-        React.createElement("h1", {}, props.pizza),
+        React.createElement('p', {}, 'Pizza:'),
+
+        React.createElement('h1', {}, props.pizza),
       ]
     ),
-    React.createElement("p", {}, props.ingredient),
+    React.createElement('p', {}, props.ingredient),
   ]);
 };
 
 const App = () => {
-  return React.createElement("div", {}, [
+  return React.createElement('div', {}, [
     React.createElement(Pizza, {
-      pizza: "The pepperoni Piza",
-      ingredient: "Mozarella Chesse, Pepperoni",
+      pizza: 'The pepperoni Piza',
+      ingredient: 'Mozarella Chesse, Pepperoni',
     }),
     React.createElement(Pizza, {
-      pizza: "Margherita Pizza",
-      ingredient: "Tomato sauce, mozzarella cheese, fresh basil, and olive oil",
+      pizza: 'Margherita Pizza',
+      ingredient: 'Tomato sauce, mozzarella cheese, fresh basil, and olive oil',
     }),
     React.createElement(Pizza, {
-      pizza: "White Pizza",
+      pizza: 'White Pizza',
       ingredient:
-        "Garlic oil, mushrooms, and fresh herbs, without tomato sauce. ",
+        'Garlic oil, mushrooms, and fresh herbs, without tomato sauce. ',
     }),
   ]);
 };
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(React.createElement(App));
