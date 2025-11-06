283be00f15ddcd0201a3c2b0d5bde5c7ba408bd3
Niloy Ro <niloy.roy@therapservices.net>
2025-11-06 15:07:08 +0600

    Vanilla React Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement(
+      "div",
+      { style: { display: "flex", gap: "100px", alignItems: "center" } },
+      [
+        React.createElement("p", {}, "Pizza:"),
+        React.createElement("h1", {}, props.pizza),
+      ]
+    ),
+    React.createElement("p", {}, props.ingredient),
+  ]);
+};
+
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Pixel Perfect Pizzas")
-  );
+  return React.createElement("div", {}, [
+    React.createElement(Pizza, {
+      pizza: "The pepperoni Piza",
+      ingredient: "Mozarella Chesse, Pepperoni",
+    }),
+    React.createElement(Pizza, {
+      pizza: "Margherita Pizza",
+      ingredient: "Tomato sauce, mozzarella cheese, fresh basil, and olive oil",
+    }),
+    React.createElement(Pizza, {
+      pizza: "White Pizza",
+      ingredient:
+        "Garlic oil, mushrooms, and fresh herbs, without tomato sauce. ",
+    }),
+  ]);
 };
 
 const container = document.getElementById("root");
