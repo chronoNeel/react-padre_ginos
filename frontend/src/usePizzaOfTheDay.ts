525027aa8e7a984ad87fb1c29297291fb3890d06
Niloy Roy <niloy55@student.sust.edu>
2025-11-13 19:39:49 +0600

    use debug added

--- frontend/src/usePizzaOfTheDay.ts
+++ frontend/src/usePizzaOfTheDay.ts
@@ -1,8 +1,14 @@
-import {useEffect, useState} from 'react';
+import {useDebugValue, useEffect, useState} from 'react';
 import {pizzaType} from './Order';
 
 export const usePizzaOfTheDay = () => {
   const [pizzaOfTheDay, setPizzaOfTheDay] = useState<pizzaType | null>(null);
+
+  useDebugValue(
+    pizzaOfTheDay
+      ? `${pizzaOfTheDay.name} and id: ${pizzaOfTheDay.id}`
+      : 'loading...'
+  );
 
   useEffect(() => {
     const fetchPizzaOfTheDay = async () => {
